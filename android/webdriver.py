b6718ada950b4d99af1076ec420949ca628b3491
Sean Wang <wangxiao8611@gmail.com>
2011-10-14 22:06:55 +0800

    finished README document and changed class name to align with other browsers

--- android/webdriver.py
+++ android/webdriver.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.remote.command import Command
 from service import Service
 
-class Android(RemoteWebDriver):
+class WebDriver(RemoteWebDriver):
 
     def __init__(self):
         self.service = Service()
